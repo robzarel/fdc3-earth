dbdb0d424f5c5ba61a95b4601b14f839d6a498b0
robzarel <robzarel@yandex.ru>
2023-04-05 12:06:46 +0200

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,26 @@ type W = Window &
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [channel, setChannel] = useState<any>();
 
   const fdc3 = useFdc3();
+
+  useEffect(() => {
+    fdc3?.getOrCreateChannel('myChannel').then((fdc3Channel: any) => {
+      setChannel(fdc3Channel);
+    })
+  },[fdc3])
+
+  useEffect(() => {
+    if (channel) {
+      const listener = channel.addContextListener('mars', (context: any) => {
+        console.log('context', context);
+        setCounter(context.id.count);
+      });
+
+      return () => listener.unsubscribe();
+    }
+  },[channel])
 
   const handleSendClick = () => {
     fdc3?.raiseIntent('ViewContact', {
@@ -26,6 +44,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>earth</h1>
       <p>counter value: {counter}</p>
       <button onClick={handleCounterClick}>increase</button>
       <button onClick={handleSendClick}>send</button>
