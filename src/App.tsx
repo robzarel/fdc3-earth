6c2c8daf42f4781e30d3b490d6c5ae94b777d9b3
robzarel <robzarel@yandex.ru>
2023-04-05 11:24:56 +0200

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+type W = Window &
+  typeof globalThis & {
+    fdc3: any;
+  };
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [isFdc3Availiable, setFdc3Availabillity] = useState(false);
+
+  useEffect(()=> {
+    const startFdc3 = () => {
+      setFdc3Availabillity(true);
+    }
+    
+    if ((window as W).fdc3) {
+      startFdc3();
+    } else {
+      window.addEventListener('fdc3Ready', startFdc3);
+    }
+    
+    return () => {
+      window.removeEventListener('fdc3Ready', startFdc3);
+    }
+  }, []);
+
+  const handleSendClick = () => {
+    if (isFdc3Availiable) {
+      (window as W).fdc3.raiseIntent('ViewContact', {
+        type: 'fdc3.contact',
+        id: { count: counter }
+      });
+    }; 
+  }
+
+  const handleCounterClick = () => {
+    setCounter((prev) => prev+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>counter value: {counter}</p>
+      <button onClick={handleCounterClick}>increase</button>
+      <button onClick={handleSendClick}>send</button>
     </div>
   );
 }
